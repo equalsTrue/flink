9d786e37d3120ab25e024923d7ab365683cb7636
david.li <david.li@starpavilion-digital.com>
2023-08-21 17:29:47 +0800

    flink 基本流程， client---->JobMaster------->TaskManager

--- src/main/java/controller/StreamWordCount.java
+++ src/main/java/controller/StreamWordCount.java
@@ -26,22 +26,26 @@ public class StreamWordCount {
         // 创建流式启动环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        // 全局禁用算子链
+        // env.disableOperatorChaining();
+
         //读取文件
         DataStreamSource<String> dataStreamSource = env.readTextFile("/Users/lichen/IdeaProjects/flink/src/main/input/WordCount.txt");
 
-        // flatMap 转化计算
+        // flatMap 转化计算, setParallelism(2) 设置并行度 ； disableChaining() 禁用算子链；startNewChain() 开启算子链; slotSharingGroup 设置slot共享组，默认是default
         SingleOutputStreamOperator<Tuple2<String, Long>> streamWordTuple = dataStreamSource.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
             String[] words = line.split(" ");
             for (String word : words) {
                 out.collect(Tuple2.of(word,1L));
             }
+//        }).returns(Types.TUPLE(Types.STRING,Types.LONG)).setParallelism(2).disableChaining().startNewChain().slotSharingGroup("1");
         }).returns(Types.TUPLE(Types.STRING,Types.LONG));
 
         // 分组
         KeyedStream<Tuple2<String, Long>, String> tuple2StringKeyedStream = streamWordTuple.keyBy(data -> data.f0);
 
         // 统计
-        SingleOutputStreamOperator<Tuple2<String, Long>> sum = tuple2StringKeyedStream.sum(1);
+        SingleOutputStreamOperator<Tuple2<String, Long>> sum = tuple2StringKeyedStream.sum(1).setParallelism(2);
 
         // 打印
         sum.print();
