a18de7a3d927fd90f19ffc97f60df52de4aa7eaf
david.li <david.li@starpavilion-digital.com>
2023-08-28 18:16:35 +0800

    flink 物理分区

--- src/main/java/controller/StreamKeyByReduce.java
+++ src/main/java/controller/StreamKeyByReduce.java
@@ -7,10 +7,13 @@ package controller;/**
 
 import model.Event;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -33,7 +36,9 @@ public class StreamKeyByReduce {
         List<Event> eventList = new ArrayList<>();
         eventList.add(new Event("jack","/pro?id=1",null));
         eventList.add(new Event("flink","/pro?id=2",null));
+        eventList.add(new Event("flink","/pro?id=2",null));
         eventList.add(new Event("jack","/pro?id=1",null));
+        eventList.add(new Event("david","/pro?id=3",null));
         eventList.add(new Event("david","/pro?id=3",null));
         eventList.add(new Event("flink","/pro?id=1",null));
         eventList.add(new Event("david","/pro?id=3",null));
@@ -50,15 +55,52 @@ public class StreamKeyByReduce {
                 return Tuple2.of(value1.f0, value1.f1 + value2.f1);
             }
         });
-        // 然后取出Count最大的元素
-        SingleOutputStreamOperator<Tuple2<String, Long>> returns = clickCount.keyBy(data -> "user").reduce(new ReduceFunction<Tuple2<String, Long>>() {
+        // 分组max 时，不能用name 做分组条件，否则判断不同的key 会分散到不同的slot中，不能比价大小,max 比较大的是大于或等于就是默认最大的
+        SingleOutputStreamOperator<Tuple2<String, Long>> maxOp = clickCount.keyBy(data -> "key").maxBy(1);
+        maxOp.print("max 2: ");
+
+        // 然后取出Count最大的元素，将所有二元数组全部放在同一个分区中，然后比较他们统计的个数，如果后面的数比之前的大就返回后面的数，如果相等也返回后面的数
+        clickCount.keyBy(data -> "user").reduce(new ReduceFunction<Tuple2<String, Long>>() {
             @Override
             public Tuple2<String, Long> reduce(Tuple2<String, Long> value1, Tuple2<String, Long> value2) throws Exception {
                 return value1.f1 > value2.f1 ? value1 : value2;
             }
-        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG)).print("reduce inner class max: ");
 
-        returns.print("max: ");
+        // reduce lambda
+       clickCount.keyBy(data -> "name").reduce((s1,s2) ->{
+            if(s1.f1 > s2.f1){
+                return s1;
+            }else {
+                return s2;
+            }
+       }).print("reduce lambda max: ");
+
+
+       // 自定义富函数，可以设置算子的状态，声明周期
+
+        SingleOutputStreamOperator<String> richOp = dataStream.keyBy(data -> data.name).map(new RichMapFunction<Event, String>() {
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                // 获取运行时上下文
+                getRuntimeContext().getTaskName();
+
+            }
+
+
+            @Override
+            public void close() throws Exception {
+                super.close();
+            }
+
+            @Override
+            public String map(Event event) throws Exception {
+                return event.name;
+            }
+        });
+
 
         env.execute();
 
---
+++ src/main/java/controller/StreamShuffle.java
@@ -0,0 +1,99 @@
+package controller;/**
+ * @program flink
+ * @description: 物理分区
+ * @author: lichen
+ * @create: 2023/08/28 11:25
+ */
+
+import model.Event;
+import org.apache.flink.api.common.functions.Partitioner;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program flink
+ * @description: 物理分区
+ * @author: lichen
+ * @create: 2023/08/28 11:25 
+ */
+public class StreamShuffle {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+// 测试数据
+        List<Event> eventList = new ArrayList<>();
+        eventList.add(new Event("mary","/pro?id=1",null));
+        eventList.add(new Event("flink","/pro?id=2",null));
+        eventList.add(new Event("flink","/pro?id=2",null));
+        eventList.add(new Event("jack","/pro?id=1",null));
+        eventList.add(new Event("david","/pro?id=3",null));
+        eventList.add(new Event("david","/pro?id=3",null));
+        eventList.add(new Event("flink","/pro?id=1",null));
+        eventList.add(new Event("david","/pro?id=3",null));
+        eventList.add(new Event("david","/pro?id=3",null));
+        eventList.add(new Event("david","/pro?id=3",null));
+        DataStreamSource<Event> dataStream = env.fromCollection(eventList);
+
+        // shuffle 随机打乱分区
+        dataStream.map(data -> data.name).returns(Types.STRING).shuffle().print().setParallelism(4);
+
+        // rebalance 平衡分区
+        dataStream.map(data -> data.name).returns(Types.STRING).rebalance().print().setParallelism(4);
+
+        // rescale 重缩放分区，将并行数据datasource 先分区，在后续的子任务根据datasource 数据分slot 处理
+        env.addSource(new RichParallelSourceFunction<Integer>() {
+            @Override
+            public void run(SourceContext<Integer> sourceContext) throws Exception {
+                // 将源数据按照奇偶性分成两个分区， 0 slot, 1 slot
+                for(int i = 1; i<= 12 ;i ++){
+                    if(i % 2 == getRuntimeContext().getIndexOfThisSubtask()){
+                        sourceContext.collect(i);
+                    }
+
+                }
+            }
+
+            @Override
+            public void cancel() {
+
+            }
+            // 输出slot(1,2) 输出偶数； 输出slot(3,4) 输出奇数
+        }).setParallelism(2).rescale().print().setParallelism(4);
+
+        // 广播输出， 设置几个并行度，就有几个并行slot 得到数据
+        dataStream.broadcast().print("广播：").setParallelism(4);
+
+        // 全局分区，将所有的数据都汇总到一个slot 上，通常用于处理完数据后合并数据，之后设置的并行度也不起作用
+        dataStream.global().print("全局").setParallelism(4);
+
+        // 自定义分区, partition 设置分区策略，若分区的数量小于后续子任务的并行度，则按照自定义的分区策略执行
+        env.fromElements(1,2,3,4,5,6,7,8,0,10).partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int num) {
+                if(key == 1){
+                    return 0;
+                }else if(key == 2){
+                    return 1;
+                }else {
+                    return 2;
+                }
+            }
+        }, new KeySelector<Integer, Integer>() {
+            @Override
+            public Integer getKey(Integer value) throws Exception {
+                return value;
+            }
+        }).print().setParallelism(6);
+
+
+        env.execute();
+
+    }
+}
